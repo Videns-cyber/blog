d97358f87688f7a2cf4d618bdd274cd1008f86e1
Anna <64422901+Videns-cyber@users.noreply.github.com>
2020-10-22 11:48:27 +0400

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,14 +1,14 @@
 let smoothJumpUp = function() {
   if (document.body.scrollTop>0 || document.documentElement.scrollTop>0) {
     window.scrollBy(0,-50);
-    setTimeout(smoothJumpUp, 5);
+    setTimeout(smoothJumpUp, 10);
   }
 }
 
 window.oncscroll = function() {
   let scrolled = window.pageYOffset;
   if (scrolled > 400) {
-    document.getElementById('upbutton').style.display = 'block';
+    document.getElementById('upbutton').style.display = 'none';
   } else {
     document.getElementById('upbutton').style.display = 'none';
   }
